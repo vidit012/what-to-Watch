d519c9d3172d589bbdaa784ba4070ade22c2fa8d
vidit012 <134963374+vidit012@users.noreply.github.com>
2025-01-15 19:06:31 +0530

    Add files via upload

---
+++ anime.d.ts
@@ -0,0 +1,12 @@
+declare type Anime = {
+    name: string;
+    producers: string;
+    episodes: number;
+    genres: string;
+    synopsis: string;
+    aired: string;
+    embedding: number[];
+  };
+  
+  export default Anime;
+  
---
+++ tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        veryDarkBlue: "#1B262C",
+        darkBlue: "#0F4C75",
+        lightBlue: "#3282B8",
+        veryLightBlue: "#BBE1FA",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      spacing: {},
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
